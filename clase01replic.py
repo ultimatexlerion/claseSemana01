5e8b87345846c4530030f7b196746c7f280adbf9
ultimatexlerion <xleriontechnology@gmail.com>
2024-08-13 22:36:09 -0500

    Ejercicio01

---
+++ clase01replic.py
@@ -0,0 +1,13 @@
+# Compuerta AND, OR, NOT
+print ("'Compuerta AND Verdadero y Verdadero' =",True and True)
+print ("'Compuerta AND Verdadero y Falso' =",True and False)
+print ("'Compuerta AND Falso y Verdadero' =",False and True)
+print ("'Compuerta AND Falso y Falso' =",False and False)
+
+print ("'Compuerta AND Verdadero y Verdadero' =",True or True)
+print ("'Compuerta AND Verdadero y Falso' =",True or False)
+print ("'Compuerta AND Falso y Verdadero' =",False or True)
+print ("'Compuerta AND Falso y Falso' =",False or False)
+
+print ("'Compuerta AND Verdadero y Verdadero' =",not False)
+print ("'Compuerta AND Verdadero y Verdadero' =",not True)
